047df190993978fa71255c774c6bf528b72ae6c3
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-16 20:21:58 +0200

    Confirming we can talk to the event store

--- Source/Head/EventsController.cs
+++ Source/Head/EventsController.cs
@@ -1,6 +1,12 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using Dolittle.Artifacts;
+using Dolittle.DependencyInversion;
+using Dolittle.Events;
+using Dolittle.Execution;
+using Dolittle.Tenancy;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Head
@@ -8,6 +14,15 @@ namespace Head
     [Route("/api/Events")]
     public class EventsController : Controller
     {
+        readonly IExecutionContextManager _executionContextManager;
+        readonly FactoryFor<IEventStore> _eventStore;
+
+        public EventsController(IExecutionContextManager executionContextManager, FactoryFor<IEventStore> eventStore)
+        {
+            _executionContextManager = executionContextManager;
+            _eventStore = eventStore;
+        }
+
         [HttpGet]
         public IActionResult Status()
         {
@@ -18,6 +33,11 @@ namespace Head
         [Route("Single")]
         public IActionResult SingleEvent()
         {
+            _executionContextManager.CurrentFor((TenantId)Guid.Parse("f79fcfc9-c855-4910-b445-1f167e814bfd"));
+            var eventStore = _eventStore();
+            var events = new UncommittedEvents();
+            events.Append(new MyEvent());
+            eventStore.Commit(events);
             return Ok();
         }
     }
