9c4b3669abeac774cff4dd4e27ad1d9048d16d1a
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-20 21:48:06 +0200

    Structuring

---
+++ Source/TheAviator/tests/a_single_microservice.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { IGiven } from '../IGiven';
+import { ScenarioContext } from '../ScenarioContext';
+import { Guid } from '@dolittle/rudiments';
+
+export class a_single_microservice implements IGiven {
+    async describe(context: ScenarioContext) {
+        context.withMicroservice('main', [Guid.parse('f79fcfc9-c855-4910-b445-1f167e814bfd')]);
+    }
+}
---
+++ Source/TheAviator/tests/single_events_committed.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import * as util from 'util';
+const asyncTimeout = util.promisify(setTimeout);
+
+import { Scenario } from '../Scenario';
+
+import { a_single_microservice } from './a_single_microservice';
+import { Guid } from '@dolittle/rudiments';
+
+export class single_events_committed extends Scenario {
+    given = a_single_microservice;
+
+    readonly first_event_committed: any = { 'uniqueIdentifier': Guid.create().toString() };
+    readonly second_event_committed: any = { 'uniqueIdentifier': Guid.create().toString() };
+
+    async when_committing_a_single_event() {
+        await this.context?.microservices.get('main')?.actions.commitEvent(Guid.parse('0e984977-1686-4036-98ef-14dc9f55f705'), this.first_event_committed);
+        return [
+            this.pause_the_head,
+            this.wait_for_two_seconds,
+            this.resume_the_head,
+            this.commit_another_event
+        ];
+    }
+
+    pause_the_head = async () => await this.context?.microservices.get('main')?.head.pause();
+    commit_another_event = async () => await this.context?.microservices.get('main')?.actions.commitEvent(Guid.parse('0e984977-1686-4036-98ef-14dc9f55f705'), this.second_event_committed);
+    resume_the_head = async () => await this.context?.microservices.get('main')?.head.resume();
+    wait_for_two_seconds = async () => await asyncTimeout(2000);
+
+    then_first_event_should_be_in_event_log = () => this.context?.microservices.get('main')?.event_log?.should_contain(this.first_event_committed);
+    then_second_event_should_be_in_event_log = () => this.context?.microservices.get('main')?.event_log?.should_contain(this.second_event_committed);
+}
