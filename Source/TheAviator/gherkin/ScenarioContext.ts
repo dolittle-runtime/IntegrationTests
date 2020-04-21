24ec1306c5553377f2fe31ffcdf1958de10b2bd6
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-21 20:13:28 +0200

    Making use of definition directly

--- Source/TheAviator/gherkin/ScenarioContext.ts
+++ Source/TheAviator/gherkin/ScenarioContext.ts
@@ -2,13 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { Microservice } from '../microservices';
+import { ScenarioContextDefinition } from './ScenarioContextDefinition';
 
 export class ScenarioContext {
     readonly name: string;
+    readonly definition: ScenarioContextDefinition;
     readonly microservices: {[key: string]: Microservice};
 
-    constructor(name: string, microservices: {[key: string]: Microservice}) {
-        this.name = name;
+    constructor(definition: ScenarioContextDefinition, microservices: {[key: string]: Microservice}) {
+        this.name = definition.name;
+        this.definition = definition;
         this.microservices = microservices;
     }
 }
