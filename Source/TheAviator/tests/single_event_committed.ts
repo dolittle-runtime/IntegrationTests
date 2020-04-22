cec8e30727227618066b8d67380b0b3dba397c21
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-22 21:48:31 +0200

    Position should be 1

--- Source/TheAviator/tests/single_event_committed.ts
+++ Source/TheAviator/tests/single_event_committed.ts
@@ -20,5 +20,5 @@ export class single_event_committed extends scenario_for_a_single_microservice {
     wait_for_one_second = async () => await asyncTimeout(1000);
 
     then_event_should_be_in_event_log = () => this.event_log?.should_contain(this.tenant, this.event_committed);
-    then_event_handler_should_have_been_handled = () => this.stream_processors?.should_have_event_handler_at_position(this.tenant, this.eventHandlerId, 2);
+    then_event_handler_should_have_been_handled = () => this.stream_processors?.should_have_event_handler_at_position(this.tenant, this.eventHandlerId, 1);
 }
