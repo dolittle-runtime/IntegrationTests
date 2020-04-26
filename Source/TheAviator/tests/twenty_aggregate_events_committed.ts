9a0ca8f07ff0db48038172a8aba3f17dc8aa8139
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-26 10:43:58 +0200

    Checking for the correct stream :)

--- Source/TheAviator/tests/twenty_aggregate_events_committed.ts
+++ Source/TheAviator/tests/twenty_aggregate_events_committed.ts
@@ -24,6 +24,6 @@ export class twenty_aggregate_events_committed extends scenario_for_a_single_mic
     }
 
     then_all_events_should_be_in_event_log = () => this.event_log?.should_contain(this.tenant, ...this._events);
-    then_all_events_should_be_in_stream_for_processor = () => this.streams?.should_contain(this.tenant, this.eventHandlerId, ...this._events);
+    then_all_events_should_be_in_stream_for_processor = () => this.streams?.should_contain(this.tenant, this.aggregateEventHandlerId, ...this._events);
     then_event_handler_should_have_handled_all = () => this.stream_processors?.should_have_event_handler_at_position(this.tenant, this.aggregateEventHandlerId, 20);
 }
