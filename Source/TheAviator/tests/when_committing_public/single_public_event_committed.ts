794e904f4411a2ec7f688e7618763df275f2cd0a
Einar Ingebrigtsen <einar@dolittle.com>
2020-04-27 20:48:26 +0200

    Added assertion for handled external event

--- Source/TheAviator/tests/when_committing_public/single_public_event_committed.ts
+++ Source/TheAviator/tests/when_committing_public/single_public_event_committed.ts
@@ -5,6 +5,7 @@ import { Guid } from '@dolittle/rudiments';
 import { scenario_for_two_microservices } from '../scenario_for_two_microservices';
 import { Streams } from '../shared/Streams';
 import { Scopes } from '../shared/Scopes';
+import { EventHandlers } from '../shared/EventHandlers';
 
 export class single_public_event_committed extends scenario_for_two_microservices {
     readonly event_committed: any = { 'uniqueIdentifier': Guid.create().toString() };
@@ -16,4 +17,5 @@ export class single_public_event_committed extends scenario_for_two_microservice
     then_event_should_be_in_event_log_of_producer_microservice = () => this.producer?.event_log?.should_contain(this.tenant, this.event_committed);
     then_event_should_be_in_public_stream_of_producer_microservice = () => this.producer?.streams?.should_be_in_public_stream(this.tenant, Streams.publicStream, this.event_committed);
     then_event_should_be_in_external_event_log_of_consumer_microservice = () => this.consumer?.streams?.should_be_in_external_event_log(this.tenant, Scopes.producerScope, this.event_committed);
+    then_external_event_handler_should_have_been_handled = () => this.consumer?.stream_processors?.should_have_event_handler_at_position(this.tenant, EventHandlers.publicEventHandlerId, 1);
 }
