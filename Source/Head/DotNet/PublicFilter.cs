27c38e214c087bad6fb6b060875615a881bd6a29
Sindre Wilting <sindrewilting@gmail.com>
2020-05-22 11:10:32 +0200

    Make public filter build with latest

--- Source/Head/DotNet/PublicFilter.cs
+++ Source/Head/DotNet/PublicFilter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading.Tasks;
 using Dolittle.Events;
 using Dolittle.Events.Filters;
@@ -19,10 +20,10 @@ namespace Head
             _logger = logger;
         }
 
-        public Task<FilterResult> Filter(CommittedEvent @event)
+        public Task<PublicFilterResult> Filter(IPublicEvent @event, EventContext eventContext)
         {
             _logger.Information($"Filtering event");
-            return Task.FromResult(new FilterResult(true));
+            return Task.FromResult(new PublicFilterResult(true, Guid.Empty));
         }
     }
 }
