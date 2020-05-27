60ce5df4fffe4f99923921c92990a0fcd6e1b18b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-05-28 00:29:34 +0200

    Add observable RunStatus, and use in BecauseOf

--- Source/TheAviator/gherkin/BecauseOf.ts
+++ Source/TheAviator/gherkin/BecauseOf.ts
@@ -3,19 +3,25 @@
 
 import { ScenarioFor } from './ScenarioFor';
 import { ScenarioContext } from './ScenarioContext';
+import { RunStatus } from './RunStatus';
 
 export class BecauseOf {
     static nothing: BecauseOf = new BecauseOf('nothing', () => { });
 
     readonly name: string;
     readonly method: Function;
+    readonly status: RunStatus;
 
     constructor(name: string, method: Function) {
         this.name = name;
         this.method = method;
+        this.status = new RunStatus();
     }
 
     async invoke(scenarioFor: ScenarioFor<ScenarioContext>): Promise<void> {
+        this.status.running();
         await this.method.apply(scenarioFor);
+        this.status.completed();
     }
 }
+
