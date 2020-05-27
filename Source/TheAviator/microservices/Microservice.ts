b95438b42706c6e17fd66da69e587b314fadf07c
Einar Ingebrigtsen <einar@dolittle.com>
2020-05-27 12:33:23 +0200

    Cleanup

--- Source/TheAviator/microservices/Microservice.ts
+++ Source/TheAviator/microservices/Microservice.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { BrokenRule } from '@dolittle/rules';
-
 import { IContainer, LogMessageWaitStrategy, IContainerEnvironment } from '../containers';
 
 import { IMicroserviceActions } from './IMicroserviceActions';
