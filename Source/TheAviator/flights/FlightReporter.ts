002698ccd43477010bbdc621b40329b3efaccc8c
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-05-28 00:29:56 +0200

    Re-render scenario in FlightReporter when status changes

--- Source/TheAviator/flights/FlightReporter.ts
+++ Source/TheAviator/flights/FlightReporter.ts
@@ -4,15 +4,77 @@
 import { IFlightReporter } from './IFlightReporter';
 import { ScenarioResult } from './reporting';
 import { Flight } from './Flight';
-import { Scenario, ScenarioEnvironment } from '../gherkin';
+import { Scenario, ScenarioEnvironment, RunState, BecauseOf } from '../gherkin';
 
 import chalk from 'chalk';
+import { merge, Observable } from 'rxjs';
+import { Transform, TransformCallback } from 'stream';
+import { Console } from 'console';
+
+class NewlineCountingStream extends Transform {
+    newlines: number = 0;
+
+    _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback) {
+        const chunkNewlines = (chunk.toString().match(/\n/g) || []).length as number;
+        this.newlines += chunkNewlines;
+        this.push(chunk, encoding);
+        callback();
+    }
+}
+
+class NewlineCounter {
+    readonly _stream: NewlineCountingStream;
+
+    constructor() {
+        this._stream = new NewlineCountingStream();
+        this._stream.pipe(process.stdout);
+    }
+
+    get console(): Console {
+        return new Console(this._stream);
+    }
+
+    get newlines(): number {
+        return this._stream.newlines;
+    }
+
+    destroy() {
+        this._stream.unpipe()
+        this._stream.destroy();
+    }
+}
+
+type OverwritingConsoleRendererCallback = (console: Console) => void;
+
+class OverwritingConsoleRenderer {
+    _previousLines: number = 0;
+    readonly _callback: OverwritingConsoleRendererCallback;
+
+    constructor(callback: OverwritingConsoleRendererCallback) {
+        this._callback = callback;
+    }
+
+    subscribeTo(...observables: Observable<any>[]) {
+        this.render();
+        merge(...observables).subscribe(() => {
+            this.render();
+        });
+    }
+    
+    private render() {
+        process.stdout.write(`\x1B[${this._previousLines}A`);
+        const counter = new NewlineCounter();
+        this._callback(counter.console);
+        this._previousLines = counter.newlines;
+        counter.destroy();
+    }
+}
 
 export class FlightReporter implements IFlightReporter {
     observe(flight: Flight): void {
-        flight.scenario.subscribe(this.outputScenario);
-        flight.environment.subscribe(this.outputEnvironment);
-        flight.recorder.scenarioResult.subscribe(this.outputScenarioResult);
+        flight.scenario.subscribe(scenario => this.outputScenario(scenario));
+        flight.environment.subscribe(environment => this.outputEnvironment(environment));
+        flight.recorder.scenarioResult.subscribe(result => this.outputScenarioResult(result));
     }
 
     private outputEnvironment(environment: ScenarioEnvironment) {
@@ -32,15 +94,22 @@ export class FlightReporter implements IFlightReporter {
             return;
         }
 
-        console.log('\n');
-        console.log(`Given: ${chalk.bold(scenario.contextName)}\n`);
-        console.log(`Feature: ${chalk.bold(scenario.specification.feature.name)}\n`);
-        console.log(`Scenario: ${chalk.bold(scenario.name)}`);
-        console.log(`  when ${scenario.specification.when.name}`);
+        const renderer = new OverwritingConsoleRenderer((console) => {
+            console.log('\n');
+            console.log(`Given: ${chalk.bold(scenario.contextName)}\n`);
+            console.log(`Feature: ${chalk.bold(scenario.specification.feature.name)}\n`);
+            console.log(`Scenario: ${chalk.bold(scenario.name)}`);
+            console.log(` ${this.becauseOfStatusIcon(scenario.specification.when)} when ${scenario.specification.when.name}`);
 
-        for (const and of scenario.specification.ands) {
-            console.log(`   ${chalk.italic('and')} ${and.name}`);
-        }
+            for (const and of scenario.specification.ands) {
+                console.log(` ${this.becauseOfStatusIcon(and)} ${chalk.italic('and')} ${and.name}`);
+            }
+        });
+        renderer.subscribeTo(scenario.specification.when.status.state, ...scenario.specification.ands.map(_ => _.status.state));
+    }
+
+    private becauseOfStatusIcon(becauseOf: BecauseOf): string {
+        return becauseOf.status.state.value == RunState.Completed ? '✔' : ' ';
     }
 
     private outputScenarioResult(scenarioResult: ScenarioResult) {
