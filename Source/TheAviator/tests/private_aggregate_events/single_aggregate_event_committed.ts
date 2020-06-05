a0d8dde4d190bb56a168f372c4b60acf29dd06e1
Sindre Wilting <sindrewilting@gmail.com>
2020-06-05 16:16:06 +0200

    Activate test for checking event handler stream processor position

--- Source/TheAviator/tests/private_aggregate_events/single_aggregate_event_committed.ts
+++ Source/TheAviator/tests/private_aggregate_events/single_aggregate_event_committed.ts
@@ -21,5 +21,5 @@ export class single_aggregate_event_committed extends ScenarioFor<a_single_micro
 
     then_event_should_be_in_event_log = () => this.context?.event_log?.should_contain(this.context?.tenant, this.event_committed);
     then_event_should_be_in_stream_for_processor = () => this.context?.streams?.should_contain(this.context?.tenant, EventHandlers.aggregateEventHandlerId, this.event_committed);
-    // then_event_handler_should_have_been_handled = () => this.context?.stream_processors?.should_have_event_handler_at_position(this.context?.tenant, EventHandlers.aggregateEventHandlerId, 1);
+    then_event_handler_should_have_been_handled = () => this.context?.stream_processors?.should_have_event_handler_at_position(this.context?.tenant, EventHandlers.aggregateEventHandlerId, 1);
 }
--- Source/TheAviator/tests/private_events/twenty_events_committed.ts
+++ Source/TheAviator/tests/private_events/twenty_events_committed.ts
@@ -29,5 +29,5 @@ export class twenty_events_committed extends ScenarioFor<a_single_microservice> 
 
     then_all_events_should_be_in_event_log = () => this.context?.event_log?.should_contain(this.context?.tenant, ...this._events);
     then_all_events_should_be_in_stream_for_processor = () => this.context?.streams?.should_contain(this.context?.tenant, EventHandlers.eventHandlerId, ...this._events);
-    // then_event_handler_should_have_handled_all = () => this.context?.stream_processors?.should_have_event_handler_at_position(this.context?.tenant, EventHandlers.eventHandlerId, 20);
+    then_event_handler_should_have_handled_all = () => this.context?.stream_processors?.should_have_event_handler_at_position(this.context?.tenant, EventHandlers.eventHandlerId, 20);
 }
